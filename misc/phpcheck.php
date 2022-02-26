064695b7dbc2d27c4c8d97b2ba6520f11ddcdf38
Jorge Mauricio <web@jorgemauricio.com>
2022-02-26 14:32:46 -0300

    Variables setup.

---
+++ misc/phpcheck.php
@@ -0,0 +1,23 @@
+<?php 
+
+echo "SERVER_ADDR (ip - servidor)= " . $_SERVER['SERVER_ADDR'] . "<br/>"; //pode ser que não funcione em todos os servidores
+echo "LOCAL_ADDR (ip - servidor)= " . $_SERVER['LOCAL_ADDR'] . "<br/>";
+echo "getenv SERVER_ADDR (ip - servidor)= " . getenv('SERVER_ADDR') . "<br/>"; //alternativa
+
+echo "DOCUMENT_ROOT= " . $_SERVER['DOCUMENT_ROOT'] . "<br/>";
+echo "dirname= " . dirname(__FILE__) . "<br/>";
+echo "PATH_SEPARATOR= " . PATH_SEPARATOR . "<br/>";
+
+echo phpinfo();
+
+
+if (function_exists("array_combine"))
+{
+	echo "Function exists";
+}
+else
+{
+	echo "Function does not exist - better write our own";
+}
+
+?>
